24f7d822ec23b49e979e0aba1a080fe269bce815
fervifane12 <fervifane12@gmail.com>
2025-11-07 19:14:34 -0500

    Handle for single quotes

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -94,7 +94,7 @@ public class Commands {
             } else if (!content.equals("'") && isInsideQuotes) {
                 argsAndComand.add(content);
             }
-            else if (!args.contains("'") && args.contains(" ")) {
+            else if (args.contains(" ")) {
                 args = args.replaceAll("\s+", " ");
                 String[] argsList = args.split(" ");
                 argsAndComand.addAll(Arrays.asList(argsList));
