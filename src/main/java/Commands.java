a02ee1c136222b76eb3e76af03082f06fe7879db
Fernando <fervifane12@gmail.com>
2025-12-01 12:55:46 -0500

    Support for escaping within quotes

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -93,7 +93,7 @@ public class Commands {
         boolean inSingleQuote = false;
         boolean inDoubleQuote = false;
 
-        if (args.isEmpty()){
+        if (args == null){
             return commandList;
         }
 
